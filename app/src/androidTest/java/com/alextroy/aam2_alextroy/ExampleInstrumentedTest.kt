e7b7e76e28d75a0ce39fffeceee8297197830d30
GitHubAlexTroy <podliza123@gmail.com>
2018-09-28 19:43:59 +0300

    hw2 xml

---
+++ app/src/androidTest/java/com/alextroy/aam2_alextroy/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.alextroy.aam2_alextroy
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.alextroy.aam2_alextroy", appContext.packageName)
+    }
+}
